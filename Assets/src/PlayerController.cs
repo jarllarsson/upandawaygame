151b805cc1fa323deac7d6ab37e2866b5b113323
Jarl Larsson <jarl.larsson@gmail.com>
2015-08-22 10:18:08 +0200

    triple jump!

--- Assets/src/PlayerController.cs
+++ Assets/src/PlayerController.cs
@@ -7,8 +7,24 @@ public class PlayerController : MonoBehaviour
     public float m_maxSpeed = 1.0f;
     public Transform m_groundCheckPoint;
     public float m_groundCheckRadius = 0.3f;
-    public int m_groundLayer;
+    public LayerMask m_groundLayer;
     private bool m_isOnGround;
+
+    public float m_jumpPower = 1.0f;
+    public float m_doubleJumpPower = 2.0f;
+    public float m_tripleJumpPower = 3.0f;
+
+    public Material m_debugMaterial;
+
+    // Jump vars
+    private int m_jumpCount=0; // current sequential jump
+    private bool m_isJumping=false; // am I jumping?
+    private float m_jumpCountCoolDown = 0.0f;
+    public float m_doubleJumpCountCoolDownLim = 0.2f;
+    public float m_holdJumpDiminishForce = 0.7f; // amount to diminish hold-jump power each tick
+    private float m_currentJumpForce = 0.0f;
+    private bool m_jumpButtonDown = false;
+    private bool m_jumpButtonReleased = true;
 
 	// Use this for initialization
 	void Start () 
@@ -19,21 +35,127 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-	
+        switch (m_jumpCount)
+        {
+            case 0:
+                m_debugMaterial.color=Color.white; break;
+            case 1:
+                m_debugMaterial.color=Color.green; break;
+            case 2:
+                m_debugMaterial.color=Color.red; break;
+        }
 	}
 
     void FixedUpdate()
     {
-        // Check if we're on ground
-        groundCheck();
-        //
-        if (m_isOnGround)
+        if (!m_jumpButtonDown)
         {
-            // Basic steering
-            Vector3 steerDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            if (steerDir.magnitude > 1.0f) steerDir.Normalize();
-            m_rbody.velocity = new Vector3(m_maxSpeed * steerDir.x, m_rbody.velocity.y, m_maxSpeed * steerDir.z);
+            m_jumpButtonReleased = true;
+            resetJumpForce();
         }
+        else
+        {
+            m_jumpButtonReleased = false;
+        }
+        m_jumpButtonDown = Input.GetAxis("Jump") > 0.0f;
+
+
+        // Check if we're on ground, only when we're still(on y-axis) or falling down
+        if (m_rbody.velocity.y <= 0.0f) groundCheck();
+        // Reset jump if we're falling to ground
+        if (m_isOnGround && m_rbody.velocity.y <= 0.0f)
+            m_isJumping = false;
+        //
+        if (m_isOnGround && !m_isJumping)
+        {
+            m_isJumping = false;
+            steer(1.0f);
+            coolDownDoubleJumpCount(); // Count the 
+            if (wasJumpBtnReleased())
+            {
+                switch (m_jumpCount)
+                {
+                    case 0:
+                        jump(m_jumpPower); break;
+                    case 1:
+                        jump(m_doubleJumpPower); break;
+                    case 2:
+                        jump(m_tripleJumpPower); break;
+                }
+            }
+        }
+        if (m_isJumping)
+        {
+            steer(0.5f);
+            // While jumping
+            m_jumpCountCoolDown = 0.0f; // reset jump counter cooldown, start afresh
+            doJump(); // having this here allows the user to hold down button for a while for higher jump!
+        }
+    }
+
+    void steer(float m_multiplier)
+    {
+        // Basic steering
+        Vector3 steerDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        if (steerDir.magnitude > 1.0f) steerDir.Normalize();
+        m_rbody.velocity = new Vector3(m_maxSpeed * m_multiplier * steerDir.x, m_rbody.velocity.y, m_maxSpeed * m_multiplier * steerDir.z);
+    }
+
+    void jump(float p_pwr)
+    {
+        if (isJumpBtnDown())
+        {
+            setToJumpStatus();
+            m_currentJumpForce = p_pwr;
+        }
+    }
+
+    void setToJumpStatus()
+    {
+        if (!m_isJumping)
+        {
+            m_isJumping = true;
+            m_jumpCountCoolDown = 0.0f;
+            m_jumpCount++;
+            if (m_jumpCount > 2) m_jumpCount = 0;
+        }
+    }
+
+    void coolDownDoubleJumpCount()
+    {
+        if (!m_isJumping)
+        {
+            m_jumpCountCoolDown += Time.deltaTime;
+            if (m_jumpCountCoolDown > m_doubleJumpCountCoolDownLim)
+                m_jumpCount = 0;
+        }
+    }
+
+
+    bool isJumpBtnDown()
+    {
+        return m_jumpButtonDown;
+    }
+
+    bool wasJumpBtnReleased()
+    {
+        return m_jumpButtonReleased;
+    }
+
+    void doJump()
+    {
+        if (isJumpBtnDown() && m_currentJumpForce>0.0f)
+        {
+            m_rbody.AddForce(Vector3.up * m_currentJumpForce);
+            //m_rbody.velocity = new Vector3(m_rbody.velocity.x, m_currentJumpForce, m_rbody.velocity.z);
+            m_currentJumpForce *= m_holdJumpDiminishForce; // diminish over time (if user holds button down, allow for small boost, but decrease it)
+            if (m_currentJumpForce < 1.0f) m_currentJumpForce = 0.0f;
+        }
+    }
+
+    void resetJumpForce()
+    {
+        m_currentJumpForce = 0.0f;
     }
 
     void groundCheck()
@@ -43,5 +165,10 @@ public class PlayerController : MonoBehaviour
             m_isOnGround = true;
         else
             m_isOnGround = false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(m_groundCheckPoint.position, m_groundCheckRadius);
     }
 }
